7e07a08c7260a582783d56561e44d89543e595f1
tuttle123 <jy3997@stern.nyu.edu>
2024-12-10 22:32:12 -0500

    Update Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -6,7 +6,6 @@ const influencers = [
   { id: 1, name: "Taylor Swift", image: "https://cdn.prod.website-files.com/61e1db1c178256e11be974b0/63f91e8829fbd0aaf9de4cb8_htygWn9bAtmW8KF52-4zwtXFzTwiA9dfZ4Q_custom_logo.png" },
   { id: 2, name: "Kanye West", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Kanye_West_at_the_2009_Tribeca_Film_Festival_%28crop_2%29.jpg/1200px-Kanye_West_at_the_2009_Tribeca_Film_Festival_%28crop_2%29.jpg" },
   { id: 3, name: "MrBeast", image: "https://upload.wikimedia.org/wikipedia/commons/c/ce/MrBeast_2023_%28cropped%29.jpg" },
-  { id: 4, name: "The Weeknd", image: "https://upload.wikimedia.org/wikipedia/commons/9/95/The_Weeknd_Cannes_2023.png" },
   { id: 5, name: "Justin Bieber", image: "https://media.gettyimages.com/id/452816718/photo/los-angeles-ca-singer-songwriter-justin-bieber-attends-the-2014-young-hollywood-awards-brought.jpg?s=612x612&w=gi&k=20&c=w6Ae-QIytODjSoZPkJ7xLxhxMjp2qn51weFqKjTYgRM=" },
   { id: 6, name: "Jake Paul", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtkPLMehAYn8Hr2rJr9Wro_yPXJF5obZgUfA&s" },
   { id: 7, name: "Cardi B", image: "https://cdn.britannica.com/85/229185-050-3CC1C44E/Cardi-B-2019.jpg" },
@@ -21,6 +20,7 @@ const influencers = [
   { id: 16, name: "Casey Neistat", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Casey_Neistat_%40_SXSW_2017_%2833229303282%29_%28cropped%29.jpg/1024px-Casey_Neistat_%40_SXSW_2017_%2833229303282%29_%28cropped%29.jpg" },
   { id: 17, name: "Joe Rogan", image: "https://i.guim.co.uk/img/media/45727c83d4615bd841d191bf4329a019ea37cd2f/1_335_2069_1242/master/2069.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=9abc3c620f00b6c4e63be993cc98e3d7" },
   { id: 18, name: "Chris Brown", image: "https://static.standard.co.uk/2023/11/08/13/09/Chris%20Brown-xasooju6.jpeg?trim=0,52,85,56&quality=75&auto=webp&width=1000" },
+  { id: 4, name: "The Weeknd", image: "https://upload.wikimedia.org/wikipedia/commons/9/95/The_Weeknd_Cannes_2023.png" },
   { id: 19, name: "Lady Gaga", image: "https://hips.hearstapps.com/hmg-prod/images/lady-gaga-attends-the-64th-annual-grammy-awards-at-mgm-news-photo-1727455427.jpg?crop=1.00xw:0.667xh;0,0.0477xh&resize=640:*" },
   { id: 20, name: "Kai Cenat", image: "https://www.hollywoodreporter.com/wp-content/uploads/2023/02/D.jpg?w=1296&h=730&crop=1" },
 ];
